da77084e8d5cabc90831e9772cec5f6f6cd7fc29
maatheusantanadev <mssantana@ecomp.uefs.br>
2024-02-22 02:01:10 -0300

    📚 Controller de Perfil Bibliotecário

--- src/BibliotecaRegia/Controller/PerfilBibliotecarioController.java
+++ src/BibliotecaRegia/Controller/PerfilBibliotecarioController.java
@@ -3,21 +3,103 @@ package BibliotecaRegia.Controller;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import BibliotecaRegia.Main;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
 public class PerfilBibliotecarioController implements Initializable {
 
     @FXML
-    private ResourceBundle resources;
+    private MenuItem alterarLivros;
 
     @FXML
-    private URL location;
+    private Button botaoSair;
 
     @FXML
-    private ImageView credencialImageView, perfilImageView, livroImageView, pesquisarImageView, sairImageView;
+    private MenuItem cadastrarLivros;
+
+    @FXML
+    private ImageView credencialImageView;
+
+    @FXML
+    private MenuItem deletarLivros;
+
+    @FXML
+    private Button devolucao;
+
+    @FXML
+    private Button emprestimos;
+
+    @FXML
+    private MenuButton gerenciarAcervo;
+
+    @FXML
+    private ImageView livroImageView;
+
+    @FXML
+    private ImageView perfilImageView;
+
+    @FXML
+    private ImageView pesquisarImageView;
+
+    @FXML
+    private Button pesquisarLivros;
+
+    @FXML
+    private ImageView sairImageView;
+
+    @FXML
+    void CadastrarLivrosOnAction(ActionEvent event) {
+        Main.navegacaoEntreTelas("cadastrarLivros");
+
+    }
+
+    @FXML
+    void alterarLivrosOnAction(ActionEvent event) {
+        Main.navegacaoEntreTelas("alterarLivros");
+
+    }
+    @FXML
+    void deletarLivrosOnAction(ActionEvent event) {
+        Main.navegacaoEntreTelas("deletarLivros");
+
+    }
+
+    @FXML
+    void devolucaoOnAction(ActionEvent event) {
+        Main.navegacaoEntreTelas("devolucoesLivros");
+
+    }
+
+    @FXML
+    void emprestimoOnAction(ActionEvent event) {
+        Main.navegacaoEntreTelas("registrarEmprestimos");
+
+    }
+
+    @FXML
+    void pesquisarOnAction(ActionEvent event) {
+        Main.navegacaoEntreTelas("telaInicial");
+
+    }
+
+
+
+
+    @FXML
+    void bttSair(ActionEvent event) {
+        if (event.getSource() == botaoSair) {
+            Main.navegacaoEntreTelas("login");
+
+        }
+
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
