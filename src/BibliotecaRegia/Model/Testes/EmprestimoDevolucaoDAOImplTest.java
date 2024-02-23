064245f39d1213790e99a783cb352b55e160131f
vitoriatanan <vitoriatanans@gmail.com>
2024-02-23 20:27:46 -0300

    Update EmprestimoDevolucaoDAOImplTest.java

--- src/BibliotecaRegia/Model/Testes/EmprestimoDevolucaoDAOImplTest.java
+++ src/BibliotecaRegia/Model/Testes/EmprestimoDevolucaoDAOImplTest.java
@@ -27,8 +27,8 @@ public class EmprestimoDevolucaoDAOImplTest {
         usuarioTeste = new Usuario("Lara Levy", "Rua A", "87877");
         usuarioTeste2 = new Usuario("Edward House", "Rua B", "87569");
 
-        livroTeste = new Livro("Livro 1", "Autor 1", "Editora 1", "Isbn 1", 2023, "Categoria 1");
-        livroTeste2 = new Livro("Livro 2", "Autor 2", "Editora 2", "Isbn 2", 2023, "Categoria 2");
+        livroTeste = new Livro("Livro 1", "Autor 1", "Editora 1", "Isbn 1", "2023", "Categoria 1");
+        livroTeste2 = new Livro("Livro 2", "Autor 2", "Editora 2", "Isbn 2", "2023", "Categoria 2");
 
 
         emprestimoDevolucaoDAO = new EmprestimoDevolucaoDAOImpl();
